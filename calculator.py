6e9bb446c1f2f7f6ac786c170a72d4a1cede6af2
sayuj1998 <sayuj21@gmail.com>
2025-08-31 13:58:50 -0600

    Updated

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import math
+
+class Calculator: 
+
+    DELTA = 0.0000000001
+
+    @staticmethod
+    def add(a, b): 
+        return 0
+    
+    @staticmethod
+    def subtract(a, b): 
+        return 0
+
+    @staticmethod
+    def multiply(a, b): 
+        return 0
+
+    @staticmethod
+    def divide(a, b): 
+        return 0
