620de0fd86f4bd6d934cb5f277ab542ee3a2aee8
Sayuj Shrestha <sayuj21@gmail.com>
2025-09-10 14:52:40 -0600

    Fixed

--- calculator.py
+++ calculator.py
@@ -6,16 +6,16 @@ class Calculator:
 
     @staticmethod
     def add(a, b): 
-        return 0
+        return a + b
     
     @staticmethod
     def subtract(a, b): 
-        return 0
+        return a - b
 
     @staticmethod
     def multiply(a, b): 
-        return 0
+        return a * b        
 
     @staticmethod
     def divide(a, b): 
-        return 0
+        return a / b
